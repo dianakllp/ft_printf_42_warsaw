f67b12c306f9476605572f2ce42e98fe48dbd23c
Diana Kolopenyuk <dkolopen@c1r5s3.42warsaw.pl>
2024-05-21 13:52:07 +0200

    fixed 1 thing

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: dkolopen <dkolopen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 16:07:07 by dkolopen          #+#    #+#             */
-/*   Updated: 2024/05/17 13:36:38 by dkolopen         ###   ########.fr       */
+/*   Updated: 2024/05/21 13:45:41 by dkolopen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 # define FT_PRINTF_H
 # include <stdio.h>
 # include <stdarg.h>
-# include <unistd.h>
+# include <unistd.h>	
 
-int			parse_str(const char *format_str, va_list args);
-int			put_hex(unsigned int nb, const char x);
-int			print_pointer(unsigned long ptr);
-int			put_pointer(unsigned long ptr);
-int			print_char(char c);
-int			print_string(char *str);
-int			put_nb(int n);
-int			print_unsigned_int(unsigned int nb);
-void		print_hex(unsigned int nb, const char x);
-static int	print_int(int nb, int len);
+int		ft_printf(const char *str, ...);
+int		parse_str(const char *format_str, va_list args);
+int		put_hex(unsigned int nb, const char x);
+int		print_pointer(unsigned long ptr);
+int		put_pointer(unsigned long ptr);
+int		print_char(char c);
+int		print_string(char *str);
+int		put_nb(int n);
+int		print_unsigned_int(unsigned int nb);
+void	print_hex(unsigned int nb, const char x);
 
 #endif
